8f81cbc33829a8a591d7e67ded2c0e83361f9d2b
HammaAnes <146767557+HammaAnes@users.noreply.github.com>
2024-11-29 10:57:10 +0100

    added crft nchlh it works

--- log_in/views.py
+++ log_in/views.py
@@ -2,11 +2,13 @@ from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from .serialization import LoginSerializer
 
+@csrf_exempt
 @api_view(['POST'])
 @renderer_classes([JSONRenderer])
 def login_views(request):
